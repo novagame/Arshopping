3d30e53ee44ddf4a12d92b80442710a4e536ad8c
Ali Ammar <ali.ammar24x7@gmail.com>
2019-05-20 21:06:33 +0530

    Initial Commit

---
+++ ARShopping/Assets/Scenes/Furnitures/SceneChangerFurnitures.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangerFurnitures : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Homepage();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void sofa()
+    {
+        SceneManager.LoadScene("Sofa");
+
+    }
+    public void chair()
+    {
+        SceneManager.LoadScene("Chair");
+    }
+    public void table()
+    {
+        SceneManager.LoadScene("Table");
+    }
+    public void TableDetails()
+    {
+        SceneManager.LoadScene("TableDetail");
+    }
+    public void SofaDetails()
+    {
+        SceneManager.LoadScene("SofaDetail");
+    }
+    public void ChairDetails()
+    {
+        SceneManager.LoadScene("ChairDetail");
+    }
+    public void Homepage()
+    {
+        SceneManager.LoadScene("Furnitures");
+    }
+    public void ordersPage()
+    {
+        SceneManager.LoadScene("OrdersPage");
+    }
+    public void cart()
+    {
+        SceneManager.LoadScene("Cart");
+    }
+}
---
+++ ARShopping/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
+
+
+public class PlaneController : MonoBehaviour
+{
+    ARPlaneManager m_ar;
+    List<ARPlane> existingPlanes = new List<ARPlane>();
+    public Text buttonText;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        m_ar = GetComponent<ARPlaneManager>();
+    }
+    void Start()
+    {
+        
+    }
+    public void TogglePlaneDetectionAndVisibility()
+    {
+        m_ar.enabled = !m_ar.enabled;
+        if (m_ar.enabled)
+        {
+            SetAllPlanesActiveOrDeactive(true);
+            buttonText.text = "Rotate / Resize ";
+        }
+        else
+        {
+            SetAllPlanesActiveOrDeactive(false);
+            buttonText.text = "Move";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SetAllPlanesActiveOrDeactive(bool value)
+    {
+        m_ar.GetAllPlanes(existingPlanes);
+        foreach(var plane in existingPlanes)
+        {
+            plane.gameObject.SetActive(value);
+        }
+
+
+    }
+}
